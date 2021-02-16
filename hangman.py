806b20b076bfb7f5dc87edafc158036c5b1a5065
Sultan Badri <46205282+SultanBadri@users.noreply.github.com>
2021-02-15 21:26:52 -0800

    first commit

---
+++ hangman.py
@@ -0,0 +1,22 @@
+import random
+from words import words
+
+def game():
+    attempts = 7
+    guessed = []
+    word = random.choice(words)
+
+    while attempts:
+        guess_state = ""
+        for char in word:
+            guess_state += "_ "
+
+        chars_to_go = guess_state.count("_")
+
+        print(f"Your word is {len(word)} characters long:", guess_state)
+        guess = input("Guess a letter in the word: ")
+
+        if guess in word:
+            print(chars_to_go)
+
+game()
